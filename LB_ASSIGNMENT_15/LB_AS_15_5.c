50fd2f304597096707f5a41b1a240d4d0419e74f
PrathmeshPawale <prathmeshpawale9114@gmail.com>
2026-06-06 22:42:38 +0530

    PRACTICE

---
+++ LB_ASSIGNMENT_15/LB_AS_15_5.c
@@ -0,0 +1,71 @@
+/*
+Problem Statement:
+Accept N numbers from user and return product of all odd elements.
+
+Input :
+15 66 3 70 10 88
+
+Output :
+45
+
+Time Complexity : O(N)
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int Product(int Arr[], int iLength)
+{
+    int iCnt = 0;
+    int iProduct = 1;
+    int bFlag = 0;
+
+    for(iCnt = 0; iCnt < iLength; iCnt++)
+    {
+        if((Arr[iCnt] % 2) != 0)
+        {
+            iProduct = iProduct * Arr[iCnt];
+            bFlag = 1;
+        }
+    }
+
+    if(bFlag == 0)
+    {
+        return 0;
+    }
+
+    return iProduct;
+}
+
+int main()
+{
+    int iSize = 0, iRet = 0, iCnt = 0;
+    int *p = NULL;
+
+    printf("Enter number of elements : ");
+    scanf("%d",&iSize);
+
+    p = (int *)malloc(iSize * sizeof(int));
+
+    if(p == NULL)
+    {
+        printf("Unable to allocate memory");
+        return -1;
+    }
+
+    printf("Enter %d elements\n",iSize);
+
+    for(iCnt = 0; iCnt < iSize; iCnt++)
+    {
+        printf("Enter element %d : ",iCnt + 1);
+        scanf("%d",&p[iCnt]);
+    }
+
+    iRet = Product(p,iSize);
+
+    printf("Product is %d",iRet);
+
+    free(p);
+
+    return 0;
+}
